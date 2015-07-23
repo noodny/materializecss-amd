4904217f9ce62e4d78b22f0facd778deeca286d6
kjakubik <krzysiek.jakubik@gmail.com>
2015-07-23 10:58:05 +0200

    Add Waves plugin to materialize object to allow for dynamic initialization

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 define([
-    'global', 'animation', 'buttons', 'cards', 'character_counter',
+    'global', 'waves', 'animation', 'buttons', 'cards', 'character_counter',
     'collapsible', 'dropdown', 'forms', 'hammerjs', 'jquery.easing',
     'jquery.hammer', 'jquery.timeago', 'leanModal', 'materialbox',
     'parallax', 'picker', 'picker.date', 'pushpin', 'scrollFire',
     'scrollspy', 'sideNav', 'slider', 'tabs', 'toasts', 'tooltip',
-    'transitions', 'velocity', 'waves'
-], function(Materialize) {
+    'transitions', 'velocity'
+], function(Materialize, Waves) {
+    Materialize.Waves = Waves;
     return Materialize;
 });
